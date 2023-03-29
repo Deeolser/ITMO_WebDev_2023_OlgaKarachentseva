5d8bc03bad9e0bf4c89aae5c7aa5b54bc6e85128
OlgaKarachentseva <deeolser@gmail.com>
2023-03-29 21:04:48 +0300

    start

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,16 +5,7 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        'soft-orange': '#e9ab53',
-        'soft-red': '#f15e50',
-        'off-white': '#fffdfa',
-        'grayish-blue': '#c5c6ce',
-        'dark-grayish-blue': '#5d5f79',
-        'very-dark-blue': '#00001a',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
