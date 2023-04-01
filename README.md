058ba147918ba4f741840a676d881e832881cb74
OlgaKarachentseva <deeolser@gmail.com>
2023-04-01 13:19:26 +0300

    JS Intro

--- README.md
+++
@@ -1,7 +0,0 @@
-# ITMO WebDew 2023 Olga Karachetseva
-
-## Homework
-
-Lesson | ToDo
-
-Demo - itmo-webdev-olga-karachentseva-homework-9-todo.surge.sh
--- main.js
+++ main.js
@@ -1,2 +1,39 @@
-import 'uno.css';
-import '@unocss/reset/tailwind.css';
+const DOM = document.getElementById.bind(document);
+
+const domInpName = DOM('impName');
+const domInpSurname = DOM('impSurname');
+const domConResult = DOM('conResult');
+
+let fuiiName = '';
+
+function add(a, b = 5) {
+  const result = a + b;
+  //console.log('add:', a, b, result);
+  //console.log(`add:, ${a} | ${b}, | ${result}`);
+  console.log('add:', { a, b, result });
+  return result;
+}
+
+domInpName.oninput = onInpNameInput;
+domInpSurname.oninput = onInpSurnameInput;
+
+function renderFullName() {
+  fuiiName = domInpName.value + ' ' + domInpSurname.value;
+
+  domConResult.textContent = fuiiName;
+};
+
+function onInpNameInput(event) {
+  console.log('onInpNameInput:', { event });
+  renderFullName();
+};
+
+function onInpSurnameInput(event) {
+  console.log('onInpSurnameInput:', { event });
+  renderFullName();
+};
+
+const addResult = add(10);
+
+console.log('addResult:', { addResult });
+console.log(domInpName, domInpSurname);
