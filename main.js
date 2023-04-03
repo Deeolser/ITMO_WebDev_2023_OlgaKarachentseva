8838970e425a39bb55ca93d9fef940104e125bb2
OlgaKarachentseva <deeolser@gmail.com>
2023-04-03 13:15:25 +0300

    JS Intro

--- main.js
+++ main.js
@@ -4,36 +4,22 @@ const domInpName = DOM('impName');
 const domInpSurname = DOM('impSurname');
 const domConResult = DOM('conResult');
 
-let fuiiName = '';
-
-function add(a, b = 5) {
-  const result = a + b;
-  //console.log('add:', a, b, result);
-  //console.log(`add:, ${a} | ${b}, | ${result}`);
-  console.log('add:', { a, b, result });
-  return result;
-}
-
-domInpName.oninput = onInpNameInput;
-domInpSurname.oninput = onInpSurnameInput;
-
-function renderFullName() {
-  fuiiName = domInpName.value + ' ' + domInpSurname.value;
-
-  domConResult.textContent = fuiiName;
-};
-
-function onInpNameInput(event) {
+domInpName.oninput = function(event) {
   console.log('onInpNameInput:', { event });
   renderFullName();
 };
 
-function onInpSurnameInput(event) {
+domInpSurname.oninput = function(event) {
   console.log('onInpSurnameInput:', { event });
   renderFullName();
 };
 
-const addResult = add(10);
+const getFullName = () => `${domInpName.value} ${domInpSurname.value}`;
 
-console.log('addResult:', { addResult });
+function renderFullName() {
+  const fullName = getFullName();
+  console.log('renderFullName:', { fullName });
+  domConResult.textContent = fullName;
+};
+
 console.log(domInpName, domInpSurname);
