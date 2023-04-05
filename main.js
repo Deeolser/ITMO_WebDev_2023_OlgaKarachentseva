253caf80a1504df24d221260e33efa83fdb6671e
OlgaKarachentseva <deeolser@gmail.com>
2023-04-05 17:55:20 +0300

    Add random color

--- main.js
+++ main.js
@@ -1,25 +1,48 @@
-const DOM = document.getElementById.bind(document);
+const appendBlock = (block) => document.getElementById('app').appendChild(block);
+const randomRange = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const hue = randomRange(0, 359);
+const saturation = randomRange(0, 100);
+const lightness = randomRange(0, 100);
+const HSL = `hsl(${hue} ${saturation}% ${lightness}%)`;
 
-const domInpName = DOM('impName');
-const domInpSurname = DOM('impSurname');
-const domConResult = DOM('conResult');
-
-domInpName.oninput = function(event) {
-  console.log('onInpNameInput:', { event });
-  renderFullName();
+const generateColor = () => Math.random() > 0.5 ? HSL : null;
+const createBlock = (x, y, size, color) => {
+  const result = document.createElement('div');
+  if (color) {
+    result.style.backgroundColor = color;
+  }
+  result.style.width = result.style.height = `${size}px`;
+  result.style.position = 'absolute';
+  result.style.left = `${x}px`;
+  result.style.top = `${y}px`;
+  return result;
 };
 
-domInpSurname.oninput = function(event) {
-  console.log('onInpSurnameInput:', { event });
-  renderFullName();
-};
+const BLOCK_SIZE = 50;
+const DIMENSION = 5;
 
-const getFullName = () => `${domInpName.value} ${domInpSurname.value}`;
+let columns = DIMENSION;
+let rows = columns * 2;
+let xPos = 0;
+let yPos = 0;
 
-function renderFullName() {
-  const fullName = getFullName();
-  console.log('renderFullName:', { fullName });
-  domConResult.textContent = fullName;
-};
+while (rows-- > 0) {
+  let line = [];
+  xPos = 0;
+  columns = DIMENSION;
+  while (columns-- > 0) {
+    const color = generateColor();
+    const block = createBlock(xPos, yPos, BLOCK_SIZE, color);
+    line.push(color);
+    xPos += BLOCK_SIZE;
+    appendBlock(block);
+  }
+  line.reverse().forEach((color) => {
+    const block = createBlock(xPos, yPos, BLOCK_SIZE, color);
+    appendBlock(block);
+    xPos += BLOCK_SIZE;
+  });
+  yPos += BLOCK_SIZE;
+}
+;
 
-console.log(domInpName, domInpSurname);
