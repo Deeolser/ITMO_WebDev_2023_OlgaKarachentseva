55de04311768c1480c8ac81db3f6897236b86643
OlgaKarachentseva <deeolser@gmail.com>
2023-04-10 13:02:39 +0300

    Add Task

--- main.js
+++ main.js
@@ -1,18 +1,59 @@
 import 'uno.css';
 import '@unocss/reset/tailwind.css';
+import Dom from './src/constants/dom.js';
+import { randomString } from './src/utils/stringUtils.js';
 
-const domBtnCreateTask = document.getElementById('btnCreateTask');
-const domPopupCreateTask = document.getElementById('popupCreateTask');
+const Tags = ['Web', 'Update', 'Design', 'Content'];
 
-domBtnCreateTask.onclick = () => {
+class TaskVO {
+  constructor(title, date, Tags) {
+    this.title = title;
+    this.date = date;
+    this.status = Tags;
+  }
+}
+
+const task = new TaskVO('Read', Date.now(), Tags);
+const getDOM = (id) => document.getElementById(id);
+const QUERY = (container, id) => container.querySelector(`[data-id="${id}"]`);
+
+const domTask = getDOM(Dom.Template.TASK);
+
+const tasks = [];
+getDOM(Dom.Button.CREATE_TASK).onclick = () => {
   console.log('> domPopupCreateTask.classList');
+
+  const domPopupCreateTask = getDOM(Dom.Popup.CREATE_TASK);
+  const domBtnConfirm = QUERY(
+    domPopupCreateTask,
+    Dom.Button.POPUP_CREATE_TASK_CONFIRM,
+  );
+  const domBtnClose = QUERY(
+    domPopupCreateTask,
+    Dom.Button.CLOSE_POPUP_CREATE_TASK,
+  );
+
   domPopupCreateTask.classList.remove('hidden');
 
-  const domBtnCloseCreateTaskPopup = document.getElementById(
-    'btnCloseCreateTaskPopup',
-  );
-  domBtnCloseCreateTaskPopup.onclick = () => {
+  const onClosePopup = () => {
     domPopupCreateTask.classList.add('hidden');
-    domBtnCloseCreateTaskPopup.onclick = null;
+    domBtnClose.onclick = null;
+    domBtnConfirm.onclick = null;
+  };
+
+  domBtnClose.onclick = () => {
+    domPopupCreateTask.classList.add('hidden');
+    domBtnClose.onclick = null;
+  };
+
+  domBtnConfirm.onclick = () => {
+    const taskVO = new TaskVO(randomString(12), Date.now(), Tags[0]);
+
+    const taskView = domTask.cloneNode(true);
+    QUERY(taskView, Dom.Template.Task.TITLE).innerText = taskVO.title;
+    domTask.parentNode.prepend(taskView);
+    tasks.push(taskVO);
+    console.log('confirm', TaskVO);
+    onClosePopup();
   };
 };
