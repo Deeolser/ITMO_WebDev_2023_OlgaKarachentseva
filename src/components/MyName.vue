3c97476cd9c3593c5d3c9adf491af186de413bf4
OlgaKarachentseva <deeolser@gmail.com>
2023-05-06 13:41:23 +0300

    vue + project settings

---
+++ src/components/MyName.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Enter your name</h1>
+  <div>
+    {{ firstName }}
+    <span style="color:red">
+      {{ lastname }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyName',
+  data: () => ({
+    firstName: 'Olga',
+    lastname: 'Karachentseva',
+  }),
+
+};
+</script>
+
+<style scoped>
+
+</style>
