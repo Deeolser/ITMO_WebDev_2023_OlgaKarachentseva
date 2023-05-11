b95d3df837cf5e086cb8a854125aaf9fcc9c4ffe
OlgaKarachentseva <deeolser@gmail.com>
2023-05-11 20:37:58 +0300

    Basic MyName component with event and passing status in props

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,49 @@ import MyName from './components/MyName.vue';
 </script>
 
 <template>
-  <MyName />
+  <select
+    name="status"
+    @change="onSelectChange"
+  >
+    <option
+      v-if="!status"
+      value="unselected"
+      selected
+    >
+      Unselected
+    </option>
+    <option
+      v-for="item in ['Ready', 'Start', 'Stop']"
+      :key="item"
+      :value="item"
+      :selected="item === status"
+    >
+      {{ item }}
+    </option>
+  </select>
+  <MyName
+    :status="status"
+    @reset="onStatusReset"
+  />
 </template>
+
+<script>
+export default {
+  data: () => ({
+    status: null
+  }),
+  methods: {
+    onSelectChange(e) {
+      console.log('>App -> onSelectChange', e.target.value);
+      this.status = e.target.value;
+    },
+    onStatusReset() {
+      console.log('>App -> onStatusReset');
+      this.status = null;
+    }
+  }
+};
+</script>
 
 <style scoped>
 
--- src/components/MyName.vue
+++ src/components/MyName.vue
@@ -5,19 +5,59 @@
     <span style="color:red">
       {{ lastname }}</span>
   </div>
+  <small>
+    <span
+      :class="{
+        'selected': !!status,
+        'unselected': !status
+      }"
+      style="display: inline-block;"
+    >
+      Status:
+    </span>
+    <span>{{ status }}</span>
+    <div>
+      <button
+        :disabled="!status"
+        :style="{
+          visibility: status ? 'visible' : 'hidden'
+        }"
+        @click="onResetClick"
+      >Reset
+      </button>
+    </div>
+  </small>
 </template>
 
 <script>
 export default {
   name: 'MyName',
+  props: {
+    status: {
+      type: String,
+      default: 'empty',
+    },
+  },
+  emits: ['reset'],
   data: () => ({
     firstName: 'Olga',
     lastname: 'Karachentseva',
   }),
-
+  methods: {
+    onResetClick() {
+      console.log('> MyName -> onResetClick');
+      this.$emit('reset');
+    }
+  }
 };
 </script>
 
 <style scoped>
+.selected {
+  color: green;
+}
 
+.unselected {
+  color: red;
+}
 </style>
