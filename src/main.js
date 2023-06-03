780353109b61f327e13c478de55a0211ed15f407
OlgaKarachentseva <deeolser@gmail.com>
2023-06-03 13:27:41 +0300

    CouchDB

--- src/main.js
+++ src/main.js
@@ -12,7 +12,23 @@ import PROVIDE from '@/constants/provides.js';
 const pb = new PocketBase(import.meta.env.VITE_SERVER_PATH);
 console.log('pb.authStore.isValid:', pb.authStore.isValid);
 
-// let db = new PouchDB('books');
+let db = new PouchDB('http://127.0.0.1:5984/books');
+db.get('book1').then((doc) => {
+  console.log('doc', doc);
+});
+db.changes({
+  since: 'now',
+  live: true,
+  include_docs: true,
+}).on('change', function(change) {
+  // handle change
+  console.log('change', change);
+}).on('complete', function(info) {
+  // changes() was canceled
+  console.log('complete', info);
+}).on('error', function(err) {
+  console.log(err);
+});
 
 createApp(AppComposition)
   .use(createPinia()
