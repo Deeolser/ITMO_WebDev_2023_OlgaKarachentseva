d04237090b2d74efa8cf925a44c0542968306589
OlgaKarachentseva <deeolser@gmail.com>
2023-07-03 18:23:50 +0300

    пагинация

--- src/App.vue
+++ src/App.vue
@@ -3,41 +3,65 @@
     <h1 class="text-2xl font-bold">
       Словарь выражений
     </h1>
+    <my-input
+        v-model="searchQuery"
+        placeholder="Поиск..."
+    />
     <div class="flex flex-row justify-between">
       <my-button
-        class="mt-4"
-        @click="showDialog"
+          class="mt-4"
+          @click="showDialog"
       >
         Добавить выражение
       </my-button>
       <MySelect
-        v-model="selectedSort"
-        :options="sortOptions"
+          v-model="selectedSort"
+          :options="sortOptions"
       />
     </div>
 
     <MyDialog v-model:show="dialogVisible">
       <DictionaryForm
-        @add="addTranslate"
+          @add="addTranslate"
       />
     </MyDialog>
     <DictionaryList
-      v-if="!isCouplesLoading"
-      :couples="couples"
-      @remove="removeTranslate"
+        v-if="!isCouplesLoading"
+        :couples="sortedAndSearchedCouples"
+
+        @remove="removeTranslate"
     />
     <div v-else>
       Загружаю....
     </div>
+    <div
+        ref="observer"
+        class="observer"
+    />
+    <!--    <div class="flex flex-row mt-[15px]">-->
+    <!--      <div-->
+    <!--          v-for="pageNum in totalPages"-->
+    <!--          :key="pageNum"-->
+    <!--          :class="{-->
+    <!--            'border-green-300 bg-green-300': page === pageNum-->
+    <!--          }"-->
+    <!--          class="border border-solid border-green-300 p-2.5"-->
+    <!--          @click="changePage(pageNum)"-->
+    <!--      >-->
+    <!--        {{ pageNum }}-->
+    <!--      </div>-->
+    <!--    </div>-->
   </div>
 </template>
 <script>
 import DictionaryForm from './components/DictionaryForm.vue';
 import DictionaryList from './components/DictionaryList.vue';
+import MyInput from './components/UI/MyInput.vue';
 
 
 export default {
   components: {
+    MyInput,
     DictionaryList, DictionaryForm
   },
   data() {
@@ -46,21 +70,52 @@ export default {
       dialogVisible: false,
       isCouplesLoading: false,
       selectedSort: '',
+      searchQuery: '',
+      page: 1,
+      limit: 10,
+      url: 'https://jsonplaceholder.typicode.com/posts',
+      totalPages: 0,
       sortOptions: [
         {value: 'title', name: 'По названию'},
         {value: 'body', name: 'По содержимому'}
       ]
     };
   },
-  watch: {
-    selectedSort(newValue) {
-      this.couples.sort((a, b) => {
-        return a[this.selectedSort]?.localeCompare(b[this.selectedSort]);
-      });
+  computed: {
+    sortedCouples() {
+      return [...this.couples].sort((a, b) => a[this.selectedSort]?.localeCompare(b[this.selectedSort]));
     },
+    sortedAndSearchedCouples() {
+      return this.sortedCouples.filter(couple => couple.title.toLowerCase().includes(this.searchQuery.toLowerCase()));
+    }
+  },
+  watch: {
+    // Сортировка
+    // selectedSort(newValue) {
+    //   this.couples.sort((a, b) => {
+    //     return a[newValue]?.localeCompare(b[newValue]);
+    //   });
+    // },
+    // Пагинация
+    // page() {
+    //   this.fetchCouples()
+    // }
   },
   mounted() {
     this.fetchCouples();
+    console.log(this.$refs.observer);
+
+    const options = {
+      rootMargin: '0px',
+      threshold: 1.0
+    };
+    const callback = (entries, observer) => {
+      if (entries[0].isIntersecting) {
+        this.fetchMoreCouples()
+      }
+    };
+    const observer = new (callback, options);
+    observer.observe(this.$refs.observer);
   },
   methods: {
     addTranslate(couple) {
@@ -76,21 +131,42 @@ export default {
     async fetchCouples() {
       try {
         this.isCouplesLoading = true;
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
-          .then(response => response.json());
-        console.log(response);
-        this.couples = response;
-
+        const query = this.url + '?_limit=' + this.limit + '&_page=' + this.page;
+        const data = await fetch(query).then(data => data.json());
+        this.couples = data;
+        // const response = await fetch(query);
+        // this.totalPages = Math.ceil(response.headers.get('X-Total-Count') / this.limit);
       } catch (e) {
         alert('Error');
       } finally {
         this.isCouplesLoading = false;
       }
-    }
+    },
+    async fetchMoreCouples() {
+      try {
+        this.page += 1;
+        this.isCouplesLoading = true;
+        const query = this.url + '?_limit=' + this.limit + '&_page=' + this.page;
+        const data = await fetch(query).then(data => data.json());
+        this.couples = [...this.couples, ...data];
+        // const response = await fetch(query);
+        // this.totalPages = Math.ceil(response.headers.get('X-Total-Count') / this.limit);
+      } catch (e) {
+        alert('Error');
+      } finally {
+        this.isCouplesLoading = false;
+      }
+    },
+    // changePage(pageNum) {
+    //   this.page = pageNum;
+    // },
   }
 };
 </script>
 
 <style scoped>
-
+.observer {
+  height: 30px;
+  background: green;
+}
 </style>
--- src/components/DictionaryList.vue
+++ src/components/DictionaryList.vue
@@ -1,20 +1,23 @@
 <template>
   <div v-if="couples.length > 0">
     <h1
-      class="mt-4"
+        class="mt-4"
     >
       <strong>Недавно добавлены</strong>
     </h1>
-    <DictionaryItem
-      v-for="couple in couples"
-      :key="couple.id"
-      :couple="couple"
-      @remove="$emit('remove', couple)"
-    />
+    <transition-group name="couple-list">
+      <DictionaryItem
+          v-for="couple in couples"
+          :key="couple.id"
+          :couple="couple"
+          @remove="$emit('remove', couple)"
+      />
+    </transition-group>
+
   </div>
   <h2
-    v-else
-    class="text-3xl"
+      v-else
+      class="text-3xl"
   >
     Добавь значения в словарь
   </h2>
@@ -36,4 +39,23 @@ export default {
 </script>
 
 <style scoped>
+.couple-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+
+.couple-list-enter-active,
+.couple-list-leave-active {
+  transition: all 0.4s ease;
+}
+
+.couple-list-enter-from,
+.couple-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+
+.couple-list-move {
+  transition: transform 0.4s ease;
+}
 </style>
