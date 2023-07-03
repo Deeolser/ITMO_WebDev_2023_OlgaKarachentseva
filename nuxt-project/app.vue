ba3916c8f629f419e258791039c4541c98ec4bcc
OlgaKarachentseva <deeolser@gmail.com>
2023-07-03 11:55:10 +0300

    Nuxt

---
+++ nuxt-project/app.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <NuxtLayout
+        :menu-links="menuLinks"
+        :name="layout"
+    >
+      <h1 style="font-family: Roboto">
+        {{ appConfig.title }}</h1>
+      <button @click="onChangeLayout">
+        Change Layout
+      </button>
+      <NuxtPage/>
+    </NuxtLayout>
+  </div>
+</template>
+<script lang="ts" setup>
+
+const LAYOUTS = ['default', 'mobile'];
+const appConfig = useAppConfig();
+const layout = ref<string>(LAYOUTS[0]);
+
+const menuLinks = reactive([
+  {name: 'Index', link: 'ROUTES.INDEX', canRender: computed(() => true)},
+  {name: 'Books', link: 'ROUTES.BOOKS', canRender: computed(() => true)},
+  {name: 'Sign In', link: 'ROUTES.SIGNIN', canRender: computed(() => true)},
+  {
+    name: 'Sign Out', link: 'ROUTES.INDEX', canRender: computed(() => true), onClick: () => {
+      console.log('SignOUT');
+    }
+  },
+]);
+
+const onChangeLayout = () => {
+  const indexOfCurrentLayout = LAYOUTS.indexOf(layout.value);
+  layout.value = LAYOUTS[(indexOfCurrentLayout + 1) % LAYOUTS.length];
+};
+</script>
---
+++ nuxt-project/nuxt.config.ts
@@ -0,0 +1,48 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+export default defineNuxtConfig({
+    ssr: true,
+    devServer: {
+        port: 3000,
+    },
+    modules: [
+        '@nuxtjs/google-fonts',
+        '@nuxtjs/apollo',
+        '@invictus.codes/nuxt-vuetify'
+    ],
+    apollo: {
+        clients: {
+            default: {
+                httpEndpoint: process.env.VITE_GRAPHQL_END_POINT,
+                httpLinkOptions: {
+                    headers: {
+                        'x-hasura-admin-secret': process.env.VITE_GRAPHQL_ADMIN_SECRET
+                    }
+                }
+            }
+        },
+    },
+    devtools: {enabled: false},
+    telemetry: false,
+    googleFonts: {
+        families: {
+            Roboto: true,
+            Lobster: true,
+            'Josefin+Sans': true,
+            Lato: [100, 300],
+            Raleway: {
+                wght: [100, 400],
+                ital: [100]
+            },
+        },
+    },
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@use "@/assets/_colors.scss" as *;'
+                }
+            }
+        }
+    }
+});
